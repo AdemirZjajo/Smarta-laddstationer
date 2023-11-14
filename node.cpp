db62c8f1133aaece8f869f9e6e66ad79dc1a92d2
Winoys <126863283+Winoys@users.noreply.github.com>
2023-11-14 16:02:50 +0100

    Skapat node.cpp
    Denna fil är avsedd att hålla hela nodens tillståndsmaskin

---
+++ node.cpp
@@ -0,0 +1,25 @@
+#include <string>
+
+// Nodens initieringsprocess börjar här
+/* Destinationsgenerering och uträkning av miniladdning görs en gång innan
+state machinen för att låta noden bestämma vilket dess första state ska bli. */
+
+
+
+
+int main() {
+  string state = "transit";
+  switch (state) {
+  case "transit":
+    break;
+  case "queue":
+    break;
+  case "charge":
+    break;
+  }
+  return 0;
+}
+
+
+// Transit state
+//
