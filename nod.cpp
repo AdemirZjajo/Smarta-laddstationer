62849674a8a79245808cd62040d6c1c81c97aae1
Winoys <126863283+Winoys@users.noreply.github.com>
2023-11-16 11:32:48 +0100

    Ändrat namn och utvecklat lite

--- nod.cpp
+++ nod.cpp
@@ -1,5 +1,12 @@
 #include <string>
 
+int xcor // Nodens x-koordinat
+int ycor; // Nodens y-koordinat
+const string zon; // Kan anta värden laddstation 1-3 samt ett default värde, kanske "0" eller bara "default"
+
+// En lista med objekt av laddsationerna som i sin tur innehåller sitt namn (eg. ls_1) och sina paths till andra laddstationer
+
+
 // Nodens initieringsprocess börjar här
 /* Destinationsgenerering och uträkning av miniladdning görs en gång innan
 state machinen för att låta noden bestämma vilket dess första state ska bli. */
@@ -8,7 +15,7 @@ state machinen för att låta noden bestämma vilket dess första state ska bli.
 
 
 int main() {
-  string state = "transit";
+  string state = "transit"; // Starttillståndet ska bestämmas utifrån  
   switch (state) {
   case "transit":
     break;
