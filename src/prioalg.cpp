816654ddf2ba90c389ca799802401263a524ab4a
Ademir Zjajo <75216401+AdemirZjajo@users.noreply.github.com>
2023-11-17 15:22:55 +0100

    Flytta runt filer.
    Försöker ordna strukturen lite, flytta laddstation1.cpp till prioalg.cpp men koden är utkommenterad.
    
    Dem andra koderna är bortagna men de finns uppe i discord om koden behövs.

---
+++ src/prioalg.cpp
@@ -0,0 +1,31 @@
+
+// TODO: Hitta ett bra interface 
+// Exempel: Vi behöver ett interface för att kunna computa och hämta våran 
+//          nuvarande state. 
+// States : transit, queue, charge 
+
+
+
+// #include <string>
+
+// class Laddstation1 {
+//   public: // Accesstyp
+// };
+
+// int main() {
+//   string state = ; // vilken laddstaionsrutt vi ska välja, beroende på vilket uppdrag noden har när den kallar på funktionen
+//   switch (state) {
+//   case "till_LS_2":
+//     return // returna listan för denna rutt
+//     break;
+//   case "till_LS_3":
+//     break;
+//   case "till_LS_4":
+//     break;
+//   }
+//   return 0;
+// }
+
+
+// // rutter från denna specifika laddstation till de tre andra, dessa kan skapas som listor eller
+// // arrayer av koordinater för hur noden ska iterera sin förflyttning
