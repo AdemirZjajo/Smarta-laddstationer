864e48fae3eee9a4d04b6ae472b94699d9c20ad6
Winoys <simonisvedala@gmail.com>
2023-12-03 15:14:52 +0100

    vbn

--- src/communication.cpp
+++ src/communication.cpp
@@ -151,46 +151,46 @@ void changeCS(string zoneCode)
   printf("changing Zone \n");
   if (zoneCode == "LADDSTATION-1")
   {
-    mesh.stop();
+    //mesh.stop();
     MESH_PREFIX = "station1";
     String MESH_PASSWORD = "station1";
     MESH_PORT = 1111;
     printf("changed LS  to LS1\n", zoneCode);
     // delay(2000);
-    mesh.init(MESH_PREFIX, MESH_PASSWORD, MESH_PORT);
+    //mesh.init(MESH_PREFIX, MESH_PASSWORD, MESH_PORT);
     mesh.update();
   }
   else if (zoneCode == "LADDSTATION-2")
   {
-    mesh.stop();
+    //mesh.stop();
     MESH_PREFIX = "station2";
     String MESH_PASSWORD = "station2";
     MESH_PORT = 2222;
     printf("changed LS to LS2\n", zoneCode);
     // delay(2000);
-    mesh.init(MESH_PREFIX, MESH_PASSWORD, MESH_PORT);
+    //mesh.init(MESH_PREFIX, MESH_PASSWORD, MESH_PORT);
     mesh.update();
   }
   else if (zoneCode == "LADDSTATION-3")
   {
-    mesh.stop();
+    //mesh.stop();
     MESH_PREFIX = "station3";
     String MESH_PASSWORD = "station3";
     MESH_PORT = 3333;
     printf("changed LS from to LS3\n", zoneCode);
     // delay(2000);
-    mesh.init(MESH_PREFIX, MESH_PASSWORD, MESH_PORT);
+    //mesh.init(MESH_PREFIX, MESH_PASSWORD, MESH_PORT);
     mesh.update();
   }
   else if (zoneCode == "LADDSTATION-4")
   {
-    mesh.stop();
+    //mesh.stop();
     MESH_PREFIX = "station4";
     String MESH_PASSWORD = "station4";
     MESH_PORT = 4444;
     printf("changed LS to LS4\n", zoneCode);
     // delay(2000);
-    mesh.init(MESH_PREFIX, MESH_PASSWORD, MESH_PORT);
+    //mesh.init(MESH_PREFIX, MESH_PASSWORD, MESH_PORT);
     mesh.update();
   }
 }
